851ccc835063664e817b9b164160b65472ef2773
Rachit Sharma <rachitsharma3642@gmail.com>
2022-07-07 14:15:45 +0530

    Added Default Location

--- script.js
+++ script.js
@@ -16,6 +16,32 @@ const datarequest = async (datas, ur) => {
   return data;
 };
 
+const firstfunction = async () => {
+  erase();
+  var datas = {
+    access_token: access_keys[1],
+  };
+
+  var data = await datarequest(
+    datas,
+    `https://api.mapbox.com/geocoding/v5/mapbox.places/Delhi.json`
+  );
+
+  var datas = {
+    lat: data.features[0].geometry.coordinates[1],
+    lon: data.features[0].geometry.coordinates[0],
+    appid: access_keys[2],
+    units: "metric",
+  };
+
+  var data2 = await datarequest(
+    datas,
+    "https://api.openweathermap.org/data/2.5/weather"
+  );
+
+  adddata(data2, "Delhi");
+};
+
 bt.addEventListener("click", async () => {
   erase();
   var datas = {
@@ -52,9 +78,13 @@ function erase() {
 
 function adddata(value, city) {
   document.querySelector("#cty").innerHTML = city;
-  document.querySelector("#tmp").innerHTML = value.main.temp;
+  document.querySelector("#tmp").innerHTML = value.main.temp
+    .toString()
+    .toUpperCase();
   document.querySelector(".description").innerHTML =
-    value.weather[0].description;
+    value.weather[0].description.toString().toUpperCase();
   document.querySelector("#hmd").innerHTML = value.main.humidity;
   document.querySelector("#wnd").innerHTML = value.wind.speed;
 }
+
+firstfunction();
